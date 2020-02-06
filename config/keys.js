97beaac015f714e3c7894bc5ffc9cb01ba1f30c8
Artem2313 <huselnykov@gmail.com>
2020-02-06 15:53:53 +0200

    new_commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: 'mongodb://artem:arteme5fw@cluster0-shard-00-00-6ml7z.mongodb.net:27017,cluster0-shard-00-01-6ml7z.mongodb.net:27017,cluster0-shard-00-02-6ml7z.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
+
